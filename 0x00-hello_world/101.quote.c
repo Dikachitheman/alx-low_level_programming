52a45c1cf7568f621fb21c95a2e8a3598963af8f
dikachitheman <dikachi.anosike@gmail.com>
2022-06-02 13:37:29 -0700

    j

---
+++ 0x00-hello_world/101.quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - print text
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, %d-%d-%d", 2015, 10, 19);
+	return (0);
+}
