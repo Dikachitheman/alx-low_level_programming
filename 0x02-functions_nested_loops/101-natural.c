be75b82436bd4d0a189992f247387723cd86d115
dikachitheman <dikachi.anosike@gmail.com>
2022-06-07 14:37:51 -0700

    l

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - stuff
+ * Description: stuff
+ * Return: stuff
+ */
+
+int sum
+int i;
+
+for (i = 0; i < 1024; i++)
+{
+	if ((i % 3 == 0) || (i % 5 == 0))
+	{
+		sum = sum + 1;
+	}
+}
+printf("%d\n", sum);
+return (0);
+}
