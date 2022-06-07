ebec633ad083fcc2df8529b886b065c7727227eb
dikachitheman <dikachi.anosike@gmail.com>
2022-06-07 11:38:37 -0700

    k

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,7 +7,7 @@ void times_table(void)
 {
 	int i, j, k;
 
-	for (i = 0; i < 10; i++))
+	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
@@ -34,3 +34,4 @@ void times_table(void)
 		}
 		_putchar('\n');
 	}
+}
