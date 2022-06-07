de02a0506cf026173f354874eadaf59f4314f4fc
dikachitheman <dikachi.anosike@gmail.com>
2022-06-07 15:20:32 -0700

    h

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: stuff
+ * Return: stuff
+ */
+
+int main(void)
+{
+	long int i, t1 = 1, t2 = 2, sum = 0;
+
+	for (i = 1; i < 50; i++)
+	{
+		printf("%ld, ", t1);
+		sum = t1 + t2;
+		t1 = t2;
+		t2 = sum;
+		if (i == 49)
+		{
+			printf("%ld\n", t1);
+		}
+	}
+	return (0);
+}
