f72ce9f9d53b2ed147d36504aa9f0901ee26dfee
dikachitheman <dikachi.anosike@gmail.com>
2022-06-07 08:53:48 -0700

    k

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *
+ * main- stuff
+ *Return: 0
+ */
+
+void print_alphabet(void)
+{
+	char k;
+
+	k = 'a';
+
+	while (k <= 'z')
+	{
+		_putchar(k);
+		k++;
+	}
+
+	_putchar('\n');
+}
