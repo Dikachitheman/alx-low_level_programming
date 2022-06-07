fe79882e0804473003fef6030d6a9a5b96a8eb34
dikachitheman <dikachi.anosike@gmail.com>
2022-06-07 08:13:28 -0700

    k

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - stuff
+ * Return: 0
+ */
+
+int main(void)
+{
+	char putchar[9] = "_putchar";
+	int x;
+
+	for (x = 0; x < 9; x++)
+	{
+		_putchar(putchar[9]);
+	}
+	_putchar('\n');
+	return (0);
+}
