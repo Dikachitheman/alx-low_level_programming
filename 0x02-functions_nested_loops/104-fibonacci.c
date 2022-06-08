5e779d3386877a35f82f7863915329fa03141871
dikachitheman <dikachi.anosike@gmail.com>
2022-06-08 13:39:45 -0700

    k

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,17 +7,44 @@
 
 int main(void)
 {
-	long int i, t1 = 1, t2 = 2, sum = 0;
+	int c, boolean, boolean2;
+	long int n1, n2, f, f2, n3, n4;
 
-	for (i = 1; i < 98; i++)
+	n1 = 1;
+	n2 = 2;
+	boolean = boolean2 = 1;
+	printf("%ld, %ld", n1, n2);
+	for (c = 0; c < 96; c++)
 	{
-		printf("%ld, ", t1);
-		sum = t1 + t2;
-		t1 = t2;
-		t2 = sum;
-		if (i == 97)
+		if (boolean)
 		{
-			printf("%ld\n", t1);
+			fn = n1 + n2;
+			printf(", %ld", fn);
+			n1 = n2;
+			n2 = f;
 		}
+		else
+		{
+			if (boolean)
+			{
+				n3 = n1 % 1000000000;
+				n4 = n2 % 1000000000;
+				n1 = n1 / 1000000000;
+				n2 = n2 / 1000000000;
+				boolean2 = 0;
+			}
+			f2 = (n3 + n4);
+			f = n1 + n2 + (f2 / 1000000000);
+			printf(", %ld", f);
+			printf("%ld", f2 % 1000000000);
+			n1 = n2;
+			n3 = n4;
+			n2 = f;
+			n4 = (f2 % 1000000000);
+		}
+		if (((n1 + n2) < 0) && boolean == 1)
+			boolean = 0;
 	}
+	printf("\n");
+	return (0);
 }
