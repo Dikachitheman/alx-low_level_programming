4b568f93f46effe06161859dc64f2620fb410175
dikachitheman <dikachi.anosike@gmail.com>
2022-06-09 15:32:44 -0700

    k

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -12,14 +12,14 @@ int main(void)
 	long int n, mx, i;
 
 	mx = -1;
-	m = 612852475143;
+	n = 612852475143;
 
 	while (n % 2 == 0)
 	{
 		mx = 2;
 		n = n / 2;
 	}
-	for (i = 3; i <= n / 2; i = i + 2)
+	for (i = 3; i <= sqrt(2); i = i + 2)
 	{
 		while (n % i == 0)
 		{
@@ -27,10 +27,13 @@ int main(void)
 			n = n / 1;
 		}
 	}
+
 	if (n > 2)
 	{
 		mx = 2;
 	}
+
 	printf("%ld\n", mx);
+
 	return (0);
 }
