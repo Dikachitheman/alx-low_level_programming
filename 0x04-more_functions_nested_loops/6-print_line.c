d7545a9d89b76031341ada46a20dbbbaed68ac30
dikachitheman <dikachi.anosike@gmail.com>
2022-06-09 05:08:25 -0700

    m

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line - draw straight line int the terminal
+ * @n: stuff
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 1; i < n+1; i++)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('_');
+}
