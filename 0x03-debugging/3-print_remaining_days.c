a4e3dfd66e1088f2050e58eb4d5f39dca83e10cc
dikachitheman <dikachi.anosike@gmail.com>
2022-06-10 00:37:29 -0700

    k

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -7,6 +7,7 @@
 /**
  * print_remaining_days - takes a date and prints how many days are
  * left in the year, taking leap years into account
+ * Description: ss
  * @month: month in number format
  * @day: day of month
  * @year: year
