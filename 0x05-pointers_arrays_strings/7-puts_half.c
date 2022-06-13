83226269233d74d88fafe352af5a84d1273ae36b
dikachitheman <dikachi.anosike@gmail.com>
2022-06-13 13:56:17 -0700

    l

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * put_half - jj
+ * @str: ll
+ */
+
+void puts_half(char *str)
+{
+	int i, n, len;
+
+	len = strlen(str);
+	if (len % 2 != 0)
+	{
+		n = (len - 1) / 2;
+		for (i = n + 1; i <= len; i++)
+		{
+			putchar(str[i]);
+		}
+	}
+	else if (len % 2 == 0)
+	{
+		for (i = len / 2; i <= len - 1; i++)
+		{
+			putchar(str[i]);
+		}
+	}
+
+	putchar('\n');
+}
