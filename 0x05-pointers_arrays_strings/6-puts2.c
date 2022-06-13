9b86b67efb1e5eb675d165e9e15bc6c1fd6bb3f4
dikachitheman <dikachi.anosike@gmail.com>
2022-06-13 13:32:14 -0700

    l

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - dd
+ * @str - ff
+ */
+
+void puts2(char *str)
+{
+	int i;
+	int len;
+
+	len = strlen(str);
+	len len - 1;
+	for (i = 0; i <= len; i += 2)
+	{
+		putchar(str[i]);
+	}
+	putchar('\n');
+}
