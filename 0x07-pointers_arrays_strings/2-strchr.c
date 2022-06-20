cea13148c7d0a9344c7592769cf64ea5fe12bf95
dikachitheman <dikachi.anosike@gmail.com>
2022-06-20 14:54:18 -0700

    l

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * *_strchr - jj
+ * @s: jj
+ * @c: jj
+ * Return: ll
+ */
+
+char *_strchr(char *s, char c)
+{
+	int n;
+
+	while (i)
+	{
+		n = *s++;
+		if (n == c)
+		{
+			return (s - 1);
+		}
+		if (n == 0)
+		{
+			return (NULL);
+		}
+	}
+}
