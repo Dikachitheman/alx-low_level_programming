ddf699f8b42bac89353b7376dea21aef0c4e2ed0
dikachitheman <dikachi.anosike@gmail.com>
2022-06-24 14:32:02 -0700

    k

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * main - multiplies two numbers
+ * @argc: argument count
+ * @argv: the argument vector
+ * Return: 1 on error, 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+
+		return (1);
+	}
+	else
+	{
+		int num1, num2, res;
+
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[2]);
+		res = num1 * num2;
+
+		printf("%d\n", res);
+
+		return (0);
+	}
+}
