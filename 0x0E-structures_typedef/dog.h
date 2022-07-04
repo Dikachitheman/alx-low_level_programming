51121f8386824c2e6320e48cf76782c48af5c93f
dikachitheman <dikachi.anosike@gmail.com>
2022-07-04 15:01:44 -0700

    l

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,8 +1,5 @@
 #ifndef DOG_H
-
 #define DOG_H
-
-
 
 /**
 
@@ -47,11 +44,6 @@ void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 
 void free_dog(dog_t *d);
-
-char *_strcpy(char *dest, char *src);
-
-int _strlen(char *s);
-
 
 
 #endif
