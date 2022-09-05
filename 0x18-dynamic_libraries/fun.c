2044f78eea389c611218d7394bd38ab445fd2166
dikachitheman <dikachi.anosike@gmail.com>
2022-09-05 15:50:38 -0700

    l

---
+++ 0x18-dynamic_libraries/fun.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "header.h"
+
+int add(int a, int b)
+{
+	int sum = a + b;
+	return (sum);
+}
+
+int sub(int a, int b)
+{
+	int min = a - b;
+	return (min);
+}
+
+int mul(int a, int b)
+{
+	int pro = a * b;
+	return (pro);
+}
+
+int div(int a, int b)
+{
+	int quo = a / b;
+	return (quo);
+}
+
+int mod(int a, int b)
+{
+	int mo = a % b;
+	return (mo);
+}
