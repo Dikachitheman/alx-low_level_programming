057c19048c76dfaf84221fa0c40801a99e127b6b
dikachitheman <dikachi.anosike@gmail.com>
2022-09-05 05:24:43 -0700

    l

---
+++ 0x18-dynamic_libraries/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - stuff
+ * Return: 0
+ */
+
+int main(void)
+{
+	char putchar[9] = "_putchar";
+	int x;
+
+	for (x = 0; x < 8; x++)
+	{
+		_putchar(putchar[x]);
+	}
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x18-dynamic_libraries/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _islower - stuff @c
+ * Description: checks for lowercase
+ * @c: to be checked
+ * Return: 1 or c
+ */
+
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
