3aed3f231a4e19c5e90b34ae9d94f12dcbf9a857
Dikachitheman <104804948+Dikachitheman@users.noreply.github.com>
2022-09-05 13:31:04 +0100

    Create 2-strlen.c

---
+++ 0x18-dynamic_libraries/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: the string to check
+ * Return: returns the length of the string
+ */
+
+int _strlen(char *s)
+{
+	int len;
+
+	len = strlen(s);
+	return (len);
+}
